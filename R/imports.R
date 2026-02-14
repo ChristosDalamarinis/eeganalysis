382be2414847489351f397602e2642a07e138551
Christos Dalamarinis <dalamarinischristos@gmail.com>
2026-02-14 23:10:36 +0100

    update

--- R/imports.R
+++ R/imports.R
@@ -4,5 +4,5 @@
 #' @importFrom utils head write.csv
 #' @importFrom graphics barplot contour hist image polygon text
 #' @importFrom grDevices rainbow
-#' @importFrom grid grid  # Note: grid is its own package!
+#' @importFrom grid grid 
 NULL

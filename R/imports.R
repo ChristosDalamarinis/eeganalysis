463346051f1e30dcb584466671c7113de09b78c9
Christos Dalamarinis <dalamarinischristos@gmail.com>
2026-02-14 22:35:10 +0100

    update

--- R/imports.R
+++ R/imports.R
@@ -2,4 +2,7 @@
 #' @importFrom grDevices adjustcolor colorRampPalette
 #' @importFrom stats median sd
 #' @importFrom utils head write.csv
+#' @importFrom graphics barplot contour hist image polygon text
+#' @importFrom grDevices rainbow
+#' @importFrom grid grid  # Note: grid is its own package!
 NULL

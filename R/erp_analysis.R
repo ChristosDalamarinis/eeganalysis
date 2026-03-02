e2b60be2d32b27908157a328998e53c82a15c33b
Christos Dalamarinis <dalamarinischristos@gmail.com>
2026-03-02 22:18:31 +0100

    updated status

--- R/erp_analysis.R
+++ R/erp_analysis.R
@@ -17,6 +17,7 @@
 #'
 #' Author: Christos Dalamarinis
 #' Date: February 2026
+#' Status: In development
 #' ============================================================================
 #' 
 #' ======================= AVERAGE EPOCHS FUNCTION ==========================
